22a82ee6acec177ea6e7f04fdf7908b47dc5b5a7
IgorLAP <ilap1036@gmail.com>
2024-06-26 14:42:35 -0300

    refactor: remove auth need to play song
    Signed-off-by: IgorLAP <ilap1036@gmail.com>

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -9,9 +9,6 @@ const useOnPlay = (songs: Song[]) => {
   const { user } = useUser();
 
   const onPlay = (id: string) => {
-    if (!user) {
-      return authModal.onOpen();
-    }
 
     player.setId(id);
     player.setIds(songs.map(song => song.id));
